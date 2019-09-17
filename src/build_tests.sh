e823306c31e6b84595205bb75682683609407e64
Benjamin Warner <benjamin.warner@onsemi.com>
2019-09-16 19:00:18 -0600

    Build test files in separate dirs.

--- src/build_tests.sh
+++ src/build_tests.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+javac HeapNode.java -d ../MaxHeapTests
+javac HeapNode.java -d ../PriorityQueueTests
+
 for test_file in MaxHeapTest_*
 do
-	javac $test_file -d ../TestNG
+	javac $test_file -d ../MaxHeapTests
+done
+
+for test_file in PriorityQueueTest_*
+do
+	javac $test_file -d ../PriorityQueueTests
 done
